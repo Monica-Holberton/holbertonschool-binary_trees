93a04712f3cd3946e50f2505e57a7ff8f95d5158
Monica-Holberton <monicafarid7@gmail.com>
2025-05-11 13:53:01 +0000

    Binary In-Order!

--- main.c
+++ main.c
@@ -29,6 +29,6 @@ int main(void)
     root->right->left = binary_tree_node(root->right, 256);
     root->right->right = binary_tree_node(root->right, 512);
 
-    binary_tree_preorder(root, &print_num);
+    binary_tree_inorder(root, &print_num);
     return (0);
 }
